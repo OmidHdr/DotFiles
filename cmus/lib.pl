642deb464364433593d857b37e7c6c17f9211a86
h0p3 <omidnet42@yahoo.com>
2024-06-19 17:32:54 +0330

    testing

---
+++ cmus/lib.pl
@@ -0,0 +1,16 @@
+/home/sim0r/Music/Good/atash - keshidi - 128 - musicsweb.ir.mp3
+/home/sim0r/Music/Good/Uzeyir-Mehdizade-Yaxsi-Olar-(128).mp3
+/home/sim0r/Music/Good/Desiigner-Panda.mp3
+/home/sim0r/Music/classic/Erik Satie - Six Gnossiennes (1889-97) Gnossienne No. 1 [www.vmusic.ir].mp3
+/home/sim0r/Music/Good/Future-Mask-Off.mp3
+/home/sim0r/Music/Good/Imagine_Dragons_Believer_320.mp3
+/home/sim0r/Music/Good/Indila-Tourner-dans-le-vide.mp3
+/home/sim0r/Music/Good/Halsey-New-Americana.mp3
+/home/sim0r/Music/Good/James Brown - It's a Man's Man's Man's World.mp3
+/home/sim0r/Music/Good/Karol G - Tusa.mp3
+/home/sim0r/Music/Good/moon_deity_neon_blade 128.mp3
+/home/sim0r/Music/Good/Nicki Minaj _ Maluma__ Myriam Fares - Tukoh Taka.mp3
+/home/sim0r/Music/Good/selena-gomez-lose-you-to-love-me.mp3
+/home/sim0r/Music/Good/shaggy_mad_mad world.mp3
+/home/sim0r/Music/Good/yasmin-levy-la-alegria.mp3
+/home/sim0r/Music/Good/KAROL G, Shakira - TQG 320 [WWW.VIPREMIX.IR].mp3
