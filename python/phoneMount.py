49bddef7904747664d7d8625baa07582bb799628
h0p3 <omidnet42@yahoo.com>
2024-10-14 19:21:57 +0330

    make force mount

--- python/phoneMount.py
+++ python/phoneMount.py
@@ -3,8 +3,8 @@ import os
 try:
     os.system('killall simple-mtpfs')
 except Exception:
-    os.system('simple-mtpfs ~/myphone')
+    os.system('simple-mtpfs -o nonempty ~/myphone')
     print("not fount") 
 
-os.system('simple-mtpfs ~/myphone')
+os.system('simple-mtpfs -o nonempty ~/myphone')
 
